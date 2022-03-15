c958494bc0187f3ba4accf3288752a8d1b575689
nikeedev <myftr12394@gmail.com>
2022-03-15 10:20:01 +0100

    first commit

---
+++ main.ts
@@ -0,0 +1,11 @@
+const version = "0.1.4";
+
+const build = "2022.03-4";
+
+function start() {
+    console.log("-----------------------------------------------------------");
+    console.log(" Javascript Game Framework v"+version+" - Build "+ build);
+    console.log("-----------------------------------------------------------");
+}
+
+start();
