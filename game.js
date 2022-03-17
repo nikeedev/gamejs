f273a1ad6cc1806791013f5f780f9d4cd1b562d6
nikeedev <myftr12394@gmail.com>
2022-03-17 09:37:01 +0100

    Updates for Game.js!

--- game.js
+++ game.js
@@ -6,5 +6,5 @@ exports.functions = functions;
 if (require.main != module) {
     console.log("\n\n-----------------------------------------------------------");
     console.log("          Game.js v" + require('./package.json').version + " - Build " + require('./package.json').build);
-    console.log("-----------------------------------------------------------");
+    console.log("-----------------------------------------------------------\n\n");
 }
--- game.ts
+++ game.ts
@@ -4,7 +4,7 @@ const functions = require('./modules/functions');
 if (require.main != module) {
     console.log("\n\n-----------------------------------------------------------");
     console.log("          Game.js v" + require('./package.json').version+ " - Build " + require('./package.json').build);
-    console.log("-----------------------------------------------------------");
+    console.log("-----------------------------------------------------------\n\n");
 }
 
 export { functions }
